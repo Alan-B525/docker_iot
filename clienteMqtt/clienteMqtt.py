4d069dc1ea31be437617b555a69cd5aec386b667
alanb <alan.nahuel525@gmail.com>
2024-04-25 14:48:48 -0300

    modificar codigo para solucion

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,22 +1,44 @@
 import asyncio, ssl, certifi, logging, os
 import aiomqtt
 
-logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s: %(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
+
+async def contador():
+    count = 0
+    while True:
+        await asyncio.sleep(3)
+        count += 1
+        logging.info(f'Contador: {count}')
 
 async def main():
-    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    tls_context.verify_mode = ssl.CERT_REQUIRED
-    tls_context.check_hostname = True
-    tls_context.load_default_certs()
+    servidor = os.getenv('SERVIDOR', 'localhost')
+    tópico_subs_1 = os.getenv('TOPICO_SUBS_1', 'topic/subs/1')
+    tópico_subs_2 = os.getenv('TOPICO_SUBS_2', 'topic/subs/2')
+    tópico_pub = os.getenv('TOPICO_PUB', 'topic/pub')
+    
+    tls_context = ssl.create_default_context(cafile=certifi.where())
 
     async with aiomqtt.Client(
-        os.environ['SERVIDOR'],
+        servidor,
         port=8883,
         tls_context=tls_context,
     ) as client:
-        await client.subscribe(os.environ['TOPICO'])
-        async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+        await asyncio.gather(
+            client.subscribe(tópico_subs_1),
+            client.subscribe(tópico_subs_2),
+            publicador(client, tópico_pub),
+            contador(),
+        )
+
+async def publicador(client, tópico):
+    count = 0
+    while True:
+        await asyncio.sleep(5)
+        count += 1
+        await client.publish(tópico, f'Contador: {count}')
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Detenido por el usuario.")
